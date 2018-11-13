340883b41e6cbb969b84c0c112969a7a92ad7129
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-13 02:49:48 +0000

    Add cleaner solution for piglatinize in comments

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -28,3 +28,22 @@ class PigLatinizer
   end
 
 end
+
+
+
+# Better code
+
+  # def piglatinize(text)
+  #   words = text.split(" ")
+  #   arr = []
+  #   words.each do |word|
+  #     parts = word.split(/([AEIOUaeiou].*)/).reject{|w| w.empty?}
+  #     puts parts
+  #     if parts.length == 1
+  #       arr << "#{parts[0]}way"
+  #     else
+  #       arr << "#{parts[1]}#{parts[0]}ay"
+  #     end
+  #   end
+  #   arr.join(" ")
+  # end
