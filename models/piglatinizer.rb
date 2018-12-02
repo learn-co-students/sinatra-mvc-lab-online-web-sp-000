9494546bc01b0758067c5aa4c231d7a6302bcdd6
aburk3 <aburk3@gmail.com>
2018-12-02 15:47:30 -0600

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,31 @@
-require 'pry'
-
 class PigLatinizer
 
-  def piglatinize(phrase)
-    rotated = phrase.downcase.split.map do |word|
-      word.chars.rotate.join + 'way'
-    end
-
-    rotated.join(' ')
+  def piglatinize(input_str)
+    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    puts x
+    x
   end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[AaEeIiOoUu]/)
+  end
+
+  def piglatinize_word(word)
+    if !consonant?(word[0])
+      word = word + "w"
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+
+
+
 end
