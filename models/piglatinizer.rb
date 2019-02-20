43370261fadea83b8694b171cebd7e707eb93429
Daniel Dawson <dannyldawson@gmail.com>
2019-02-20 02:51:10 -0800

    Refactor #piglatinize_word, add #starting_vowel?

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,37 +1,24 @@
 class PigLatinizer
-
-  def piglatinize(input_str)
-    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  def piglatinize(input)
+    input.split(' ').size == 1 ? piglatinize_word(input) : piglatinize_sentence(input)
   end
 
   private
 
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
+  def starting_vowel?(word)
+    word[/^[AEIOUaeiou]/]
   end
 
   def piglatinize_word(word)
-    # word starts with vowel
-    if !consonant?(word[0])
-      word = word + "w"
-    # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
+    if starting_vowel?(word)
+      word << 'way'
     else
-      word = word.slice(1..-1) + word.slice(0)
+      vowel_index = word.index(word[/[AEIOUYaeiouy]/]) # Find first vowel or y in word
+      word[vowel_index..-1] << word[0..vowel_index - 1] << 'ay'
     end
-    word << "ay"
   end
 
   def piglatinize_sentence(sentence)
-    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+    sentence.split(' ').collect { |word| piglatinize_word(word) }.join(' ')
   end
-
-
-
-
 end
