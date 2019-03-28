639ab8064d0458fd35a077023558bf1f9336e5f7
Ariel <arielord@buffalo.edu>
2019-03-28 22:34:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+  post '/piglatinize' do
+    params[:user_phrase].split.collect do |word|
+    @piglatinized = PigLatinizer.new(params[:user_phrase])
+    @piglatinized = @piglatinized.transform
+    erb :piglatinize
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+class PigLatinizer
+  attr_accessor :input
+  
+  def initialize(input)
+    @input = input
+  end
+  
+  def first_vowel
+    @input.scan(/[aieouAIEOU]/).first
+  end
+  
+  def index_of_fv
+    @input.index(first_vowel)
+  end
+  
+  def find_consonants_before_vowel
+    @input[0...index_of_fv]
+  end
+  
+  def transform
+    if index_of_fv == 0
+      piglatin = @input + "yay"
+    else
+      piglatin = @input[index_of_fv..] + find_consonants_before_vowel + "ay"
+    end
+    piglatin
+  end
+end
