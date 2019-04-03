be89c5dbe56e7a112ac26e4f476452145b246aab
Diana Badas <diana.badas@gmail.com>
2019-04-03 10:20:29 +0000

    copy flatiron solution

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @text = params[:user_phrase]
-    @piglatinizer = PigLatinizer.new
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
 
-    erb :piglatinize
+    erb :results
   end
 end
