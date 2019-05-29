82ebf53290674d9d4c0acce3215ef85b5a9edee6
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-29 06:31:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require_relative 'config/environment'
-require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
 
   post '/piglatinize' do
-    # @result=PigLatinizer.new(params[:user_phrase])
+    a=PigLatinizer.new
+    @result=a.piglatinize(params[:user_phrase])
+
     erb :piglatinize
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,33 @@
 class PigLatinizer
-  # attr_accessor :word
 
-  # def initialize (word)
-  #   @word=word
-  # end
-
-  def piglatinize(word)
-    new_word=word.split('')
-    while !new_word.first.match(/[aeoiu]/)
-      new_word.push(new_word.first).shift
+  def piglatinize(input)
+    if input.split.size==1
+      individual(input)
+    else
+      eachWord(input)
     end
-    new_word.push("ay").join('')
+  end
+
+  
+  def individual(word)
+    new_word=word.split('')
+    if new_word.first.match(/[aeoiu]/i)
+      new_word.push("way")
+    else
+      while !new_word.first.match(/[aeoiu]/)
+        new_word.push(new_word.first).shift
+      end
+      new_word.push("ay")
+    end
+    new_word.join
+  end
+
+  def eachWord(sentence)
+    arr=sentence.split(" ")
+    new_arr=arr.collect do |item|
+      individual(item)
+    end
+    new_arr.join(" ")
   end
 
 end
