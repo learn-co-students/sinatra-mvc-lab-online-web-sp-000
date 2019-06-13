0f71b4daae073edaab02c9dad94fa0cc35edb7ab
Brian Simonton <bsimonton78@gmail.com>
2019-06-13 16:39:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    pl = PigLatinizer.new
-    @piglatin = pl.piglatinize(params[:user_phrase])
+    piglatin = PigLatinizer.new
+    @piglatin = piglatin.piglatinize(params[:user_phrase])
     erb :results
   end
 
