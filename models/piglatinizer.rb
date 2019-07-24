072163925a253c22bfaf9be997f4baca06143e92
Meisuleen Kased <meisuleen@gmail.com>
2019-07-24 13:35:22 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,18 @@
 class PigLatinizer
 
+  def piglatinize(input)
+    # Your app will take in a string from a user through a form, convert it to pig latin,
+    # and return the string to the user.
 
+    #if first letter in word starts with a consonant move first letter of word to the end and
+    # add 'ay' at the end of the word
+
+    #if the first letter in the word starts with a consonant cluster, move consonant cluster of word to the end and
+    # add 'ay' at the end of the word
+
+    #For words that begin with vowel sounds, one just adds "way" or "yay" to the end (or just "ay") ||
+    # An alternative convention for words beginning with vowel sounds, one removes the initial vowel(s)
+    # along with the first consonant or consonant cluster.
+  end
 
 end
