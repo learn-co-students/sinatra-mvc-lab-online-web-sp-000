f59565537cd21fc8c6eb19726ab4d96653ab6976
Lauren McCoy <lmccoy8911@gmail.com>
2019-08-17 01:23:57 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,10 @@
+class Piglatinizer
+  
+  def initialize(string)
+    string 
+  end 
+  
+  
+# Pig Latin is a made-up language formed from English by transferring the initial consonant or consonant blend (for example, "ch" or "str") of each word to the end of the word along with the syllable "-ay". If the word begins with a vowel sound, then we add the sound of "way" to the end of the word.
+  
+end 
