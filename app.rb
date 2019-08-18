406d2d1896a24156798e22c78e0119f9eb960cfb
Lauren McCoy <lmccoy8911@gmail.com>
2019-08-18 17:16:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   
   get '/' do 
+    @user_input = params[:user_phrase]
     erb :user_input 
+  end 
+  
+  post '/piglatinize' do
+    @user_input = PigLatinizer.new.piglatinize(params[:user_phrase])
+    erb :piglatinize
   end 
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,51 @@
-require 'pry'
 class PigLatinizer
   
-  def piglatinize(string)
+  def piglatinize(input) 
+    new_word = input.split(" ")
      
-    if string.start_with?(/aeiou/) 
-      new_string.prepend("way")
-    else
-      new_string = string.chars
-      take = new_string.delete_at(0)
-      new_string << take 
-      new_string.push("ay")
-      new_string.join("")
-      
-    end
+    if new_word.length > 1 
+      piglatinize_sentence(new_word)
+    else 
+      piglatinize_word(input)
+    end 
   end 
   
+  def piglatinize_word(word)
+    if word.start_with?(/[aeiouAEIOU]/) 
+      word.insert(-1, "way")
+    elsif is_consonant?(word[0]) && is_consonant?(word[1]) && is_consonant?(word[2])
+        new_word = word.chars
+        take = new_word.delete_at(0)
+        take2 = new_word.delete_at(0)
+        take3 = new_word.delete_at(0)
+        new_word << take << take2 << take3
+        new_word.push("ay")
+        new_word.join("")
+    elsif is_consonant?(word[0]) && is_consonant?(word[1])
+      new_word = word.chars
+      take = new_word.delete_at(0)
+      take2= new_word.delete_at(0)
+      new_word << take << take2 
+      new_word.push("ay")
+      new_word.join("")
+    else
+      new_word = word.chars
+      take = new_word.delete_at(0)
+      new_word << take 
+      new_word.push("ay")
+      new_word.join("")
+    end 
+  end 
   
-# Pig Latin is a made-up language formed from English by transferring the initial consonant or consonant blend (for example, "ch" or "str") of each word to the end of the word along with the syllable "-ay". If the word begins with a vowel sound, then we add the sound of "way" to the end of the word.
+  def is_consonant?(letter)
+     !letter.match(/[aeiouAEIOU]/)
+  end 
   
+  def piglatinize_sentence(string)
+  
+      string.map do |word|
+        word = word.to_s 
+        piglatinize_word(word)
+        end.join(" ") 
+  end 
 end 
