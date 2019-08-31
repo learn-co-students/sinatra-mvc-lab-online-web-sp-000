aba804b64f4b7ca933de94c0411ebff73b6e415c
kevin lang <langkevina@gmail.com>
2019-08-31 22:36:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+      erb :user_input
+    end
+
+    post '/piglatinize' do
+      pigL = PigLatinizer.new
+      phrase = params[:user_phrase]
+      @pigLat = pigL.piglatinize(phrase)
+      erb :result
+    end
+
+      # get '/' do
+      #   erb :user_input
+      # end
+      #
+      # post '/piglatinize' do
+      #   pl = PigLatinizer.new
+      #   @piglatin = pl.piglatinize(params[:user_phrase])
+      #   erb :results
+      # end
 end
