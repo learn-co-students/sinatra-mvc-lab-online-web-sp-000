1f370d81a135503c7bdaab899812688a83c452e5
Frederic Silber <freddy.silber@cloudcoach.com>
2019-09-11 01:35:03 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,25 +1,29 @@
 require 'pry'
 
   class PigLatinizer
-
     def piglatinize(word)
-     return word if %w[and an in].include?(word) #one syllable exceptions
-    #  return "Iway" if word === "I" || word === "i"
-     letters = word.split("")
-     letters.keep_if {|letter| letter != "."}
-     if letters.size > 1
-       until vowel?(letters[0])
-         letters << letters.shift
-       end
-    #  else
-    #    if letters == "I" || "i"
-    #
-    #    end
-
-       letters  << "ay"
-     end
-     letters.join
-   end
+      return word if %w[and an in].include?(word)
+      letters = word.split("")
+      
+    end
+  #   def piglatinize(word)
+  #    return word if %w[and an in].include?(word) #one syllable exceptions
+  #   #  return "Iway" if word === "I" || word === "i"
+  #    letters = word.split("")
+  #    letters.keep_if {|letter| letter != "."}
+  #    if letters.size > 1
+  #      until vowel?(letters[0])
+  #        letters << letters.shift
+  #      end
+  #   #  else
+  #   #    if letters == "I" || "i"
+  #   #
+  #   #    end
+   #
+  #      letters  << "ay"
+  #    end
+  #    letters.join
+  #  end
 
     def to_pig_latin(phrase)
      words = phrase.split(" ")
