e7b45478bc6bef0f250a35b24689956f2ffce080
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-09-24 12:45:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,8 +4,14 @@ require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
 
     get '/' do
-    
     erb :user_input
+    end
+
+    post '/piglatinize' do
+  
+    @piglatinizer = PigLatinizer.new.piglatinize(params[:user_phrase])
+   
+    erb :piglatinize
     end
 
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,31 +1,32 @@
 class PigLatinizer
 
-    attr_accessor :text
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
 
-    def initialize
-        @text = text
-    end
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
 
-    def piglatinize(text)
-        a = text.split
-        b = a.map {|text| piglatinize_word(text)}
-        b.join(" ")
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
     end
+    word << "ay"
+  end
 
-    def piglatinize_word(text)  
-        first_letter = text[0]
-            if ["a", "e", "i", "o", "u"].include?(first_letter)
-                "#{text}way"
-            elsif
-                consonants = []
-                consonants << text[0]
-            if ["a", "e", "i", "o", "u"].include?(text[1]) == false
-                    consonants << text[1]
-            if ["a", "e", "i", "o", "u"].include?(text[2]) == false
-                      consonants << text[2]
-                end
-            end
-            end
-        "#{text[consonants.length..-1]}#{first_letter}ay"
-    end
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
 end
