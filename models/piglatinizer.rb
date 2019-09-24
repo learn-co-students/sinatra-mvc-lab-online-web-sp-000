9abacac6b4cc1426139bd5b0fbdc793df4f0124c
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-09-23 23:39:00 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,32 @@
+class PigLatinizer
+
+    attr_accessor :text
+
+    def initialize
+        @text = text
+    end
+
+    def piglatinize(text)
+        a = text.split
+        b = a.map {|text| piglatinize_word(text)}
+        b.join(" ")
+    end
+
+    def piglatinize_word(text)  
+        first_letter = text[0]
+            if ["a", "e", "i", "o", "u"].include?(first_letter)
+                "#{text}way"
+            elsif
+                consonants = []
+                consonants << text[0]
+                ["a", "e", "i", "o", "u"].include?(text[1]) == false
+                    consonants << text[1]
+            elsif
+            ["a", "e", "i", "o", "u"].include?(text[2]) == false
+                      consonants << text[2]
+            end 
+        end
+        "#{text[consonants.length..-1]}#{first_letter}ay"
+        end 
+    end
+end
