466fae7f9f64d929858d06845a2a817836f42a4b
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-08 14:34:55 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 
 require 'pry'
 class PigLatinizer
-attr_accessor :text, :new, :arr
+attr_accessor :text, :new, :arr, :latin
 
 def initialize
 
@@ -10,17 +10,29 @@ end
 def piglatinize(text)
   @new = []
   @arr = text.split
-  @arr.each do |ne|
-    @new << ne.split
-    # ne.push(ne[0])
-    # ne[0].delete
-    # ne.push(ne[0])
-    # ne[0].delete
+  @arr.each do |word|
+    x = word.to_s
+    if x.match(/\b[AEIOUaeiou][a-z]*\b/)
+       @y = x.scan(/\w/)
 
+        2.times do
+        @y.insert(-1, @y.delete_at(0))
+        end
+        @latin = "#{@y.join("")}way"
+        binding.pry
 
   end
-  @new
 end
 
+# piglatinize("hello there person")
 
+# def starts_with_a_vowel?(word)
+#     if word.match(/\b[AEIOUaeiou][a-z]*\b/)
+#       true
+#     else false
+#     end
+# end
+
+
+end
 end
