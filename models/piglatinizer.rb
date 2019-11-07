2624c558e227d733bae58d166e2271e405581f71
Andrew Silva <andrewsilva9@gmail.com>
2019-11-07 04:39:59 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,6 @@
 class PigLatinizer
   
-  def piglatinize(word)
+  def piglatinize_word(word)
     
     first_letter_array = ["a", "e", "i", "o", "u"]
     first_letter = word[0].downcase
@@ -8,23 +8,35 @@ class PigLatinizer
     if first_letter_array.include?(first_letter)
       "#{word}way"
     else
-      binding.pry
       consonants = []
       if !first_letter_array.include?(word[0]) && !first_letter_array.include?(word[1]) && !first_letter_array.include?(word[2]) 
         consonants << word[0]
         consonants << word[1]
         consonants << word[2]
-        word = word.slice!(3..-1) + word[0] + "ay"
+        word = word.slice!(3..-1) + word[0..2] + "ay"
       elsif !first_letter_array.include?(word[0]) && !first_letter_array.include?(word[1])
         consonants << word[0]
         consonants << word[1]
-        word = word.slice!(2..-1) + word[0] + "ay"
+        word = word.slice!(2..-1) + word[0..1] + "ay"
       elsif !first_letter_array.include?(word[0])
         consonants << word[0]
         word = word.slice!(1..-1) + word[0] + "ay"
       end
     end
-    word
+  end 
+  
+  def piglatinize_sentence(sentence)
+    array = sentence.split(" ")
+    new_array = array.map { |x| piglatinize_word(x) }
+    new_array.join(" ")
+  end
+  
+  def piglatinize(input)
+    if input.length > 1
+      piglatinize_sentence(input)
+    else
+      piglatinize_word(input)
+    end
   end 
 
 end
