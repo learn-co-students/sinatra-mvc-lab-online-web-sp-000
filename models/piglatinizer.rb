3205a9257f420f113acab49b9e713e1e88109ed8
Christopher Dent <christopherdent01@gmail.com>
2019-12-08 00:11:16 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,28 +1,37 @@
+require 'pry'
+
 class PigLatinizer
   
-  def piglatinize(word)
-    return word if %w[and an in].include?(word)
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 1 
-      until vowel?(letters[0])
-      letters << letters.shift
+ 
+  def piglatinize(string)
+     a = string.split(" ")
+     b = a.map {|word| piglatinize_word(word)}
+     b.join(" ")
+  end
+
+
+  def piglatinize_word(word)
+     vowels_array = ['a', 'e', 'i', 'o', 'u']
+     cons_array = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'] 
+     first_letter = word[0].downcase
+   
+    if vowels_array.include?(first_letter)
+      word + "way"
+    else 
+      
+      consonants = [] 
+      consonants << word[0]
+        if !vowels_array.include?(word[1])
+          consonants << word[1]
+          
+          if !vowels_array.include?(word[2])
+            consonants << word[2]
+            
+          end 
+        end 
+      
+       "#{word[consonants.length..-1] + consonants.join + "ay"}"
     end 
-    
-    letters << "ay" 
-  end 
-  letters.join 
-end 
-
-def to_pig_latin(text)
-  words = text.split(" ")
-  words.map! {|word| piglatinize(word)}
-  words.join(" ")
-end 
-
-def vowel?(letter)
-  letter.downcase
-  letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-end 
+  end
 
 end 
