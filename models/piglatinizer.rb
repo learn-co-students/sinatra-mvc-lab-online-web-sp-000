30aa2b9e436ec00d60989bb404e9bddf2aa57c7b
Oana Berentan <oana.berentan@yahoo.com>
2019-12-27 18:40:31 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,35 @@
+class PigLatinizer
+  attr_reader :text, :user_input, :words
+
+  def piglatinize(sentence)
+    words = split_sentence(sentence)
+
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u A E I O U]
+    consonants = alpha - vowels
+
+    changed_words = words.collect do |word|
+      if vowels.include?(word[0])
+        word + 'way'
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1]) && consonants.include?(word[2])
+        word[3..-1] + word[0..2] + 'ay'
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1])
+        word[2..-1] + word[0..1] + 'ay'
+      elsif consonants.include?(word[0].downcase)
+        word[1..-1] + word[0] + 'ay'
+      else
+      end
+    end
+
+    pig_me = ""
+    changed_words.each do |word|
+      pig_me << word + " "
+    end
+    pig_me[0...-1]
+  end
+
+  def split_sentence(sentence)
+    words = sentence.split(' ')
+  end
+
+end
