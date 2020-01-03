13e90651743a778eade8c79014d97fa8eeebc022
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-01-03 15:26:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  erb :user_input
+end
+
+post '/piglatinize' do
+  @user_phrase = PigLatinizer.new.to_pig_latin(params[:user_phrase])
+  erb :result
+end
 end
