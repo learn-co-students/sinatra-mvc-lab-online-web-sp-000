5ec757c86bb57acb86bf296da9e768a8b8a4c247
Chris Cheong <chrisxcheong@gmail.com>
2020-01-29 18:23:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
 
   post "/piglatinize" do
-    @pig_text = PigLatinizer.new(params[:user_input])
+    @pig_text = PigLatinizer.new
+    @pig_text = @pig_text.to_pig_latin(params[:user_phrase])
     erb :piglatinize
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,11 @@
 class PigLatinizer
-  attr_accessor :text
-
-  def initialize(text)
-    @text = text
-  end
 
   def piglatinize(word)
     vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O","U"]
     moved_char = ""
     if vowels.include? word[0]
-      word << word[0]
-      word = word[1..-1]
-      word + moved_char + "way"
+      word + "way"
     else
-      constants = ""
       while !vowels.include? word[0]
         moved_char << word[0]
         word = word[1..-1]
@@ -22,8 +14,8 @@ class PigLatinizer
     end
   end
 
-  def to_pig_latin
-    @text.split.collect{|word| piglatinize(word)}.join(" ")
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
   end
 
 end
