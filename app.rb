061830701b6498a80e7e194f4e07bfafcb00b5d8
Anthony Tilelli <anthony@tilelli.me>
2020-02-01 23:19:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
-
+require "pry"
 class App < Sinatra::Base
+  get "/" do
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    converter = PigLatinizer.new
+    @piglatin = converter.piglatinize(params[:user_phrase])
+    erb :piglatinize
+  end
 end
