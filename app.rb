3b37e46e4283098057aa45e8a81b4bb4a7dd8404
Will Carter <will.carter@fergusllc.com>
2020-02-26 14:29:44 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @piglatin_text = PigLatinizer.new()
+        erb :results
+    end
 end
