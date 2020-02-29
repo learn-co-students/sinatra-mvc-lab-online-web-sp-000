75e81af63da34faaaafcd50baa2e68bac1209cc9
Michael Carroll <carrollm2@gmail.com>
2020-02-29 04:11:51 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,18 @@
 class PigLatinizer
-  attr_reader :text
 
-  def initialize(text)
-    @text = text.downcase
-  end
+  def piglatinize(word)
 
-  def convert_to_pig_latin
-    text.reverse
+    vowel_list = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    split_index_start = word.index(/[aeiouAEIOU]/)
+
+    if vowel_list.include?(word.split('').first)
+      pig_latin_end_of_word = 'way'
+    else
+      pig_latin_end_of_word = 'ay'
+    end
+
+    pig_latin_word = word.slice(split_index_start..-1) + word.slice(0, split_index_start) + pig_latin_end_of_word
+
   end
 
 end
