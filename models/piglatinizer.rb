85793b0a592513f3a869152838ac662b091ff478
CoderJay06 <jaycru06@yahoo.com>
2020-03-12 00:04:47 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,18 @@
 class PigLatinizer 
+   attr_accessor :word
+
+   def initialize
+      @word = word 
+   end 
+
+   def PigLatinize
+      # takes care of parsing the string into words
+      # then putting the piglatinized words back
+      # together.
+   end 
+
+   def PigLatinize_word 
+      #  take care of piglatinizing individual words.
+   end 
 
 end 
