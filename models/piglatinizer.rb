6684533a4d857eff4e1cb22e8eab86028e9ad36b
Alvin Coulson <aaacoulson@gmail.com>
2020-05-07 14:53:42 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,10 +3,11 @@ require 'pry'
 class PigLatinizer
    
     def piglatinize(words)
-        # binding.pry
         pl = words.split.map do |t|
             if t.start_with?(/[AEIOUaeiou]/)
                 "#{t}" + "way"
+            elsif !t.match(/[AEIOUaeiou]/)
+                "#{t}" + "ay"
             else
                 t.delete_prefix(t.slice(0...t.index(/[aeiou]/))) + t.slice(0...t.index(/[aeiou]/)) + "ay"
             end  
