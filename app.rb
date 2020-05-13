6f999068f7ea0faaea0b9b115cb3585d1a382f4b
mlgvla <mlgerardvla@gmail.com>
2020-05-13 14:49:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
+
+
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        pl_obj = PigLatinizer.new
+        @piglatin = pl_obj.piglatinize(params[:user_phrase])
+
+
+        erb :pigged
+    end
 end
