f0cb5826c0e5730aece65faaa7f10c0ee3298dcb
Joseph Kao <josephtkao@gmail.com>
2020-08-04 21:59:20 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,16 @@
 class PigLatinizer
-  def initialize(input)
+  def piglatinize(input)
     processed = input.split(" ")
     output = []
     processed.each do |word|
-      if word[0].scan(/[aeiou]/).count > 0
+      if word[0].scan(/[aeiouAEIOU]/).count > 0
         new_word = word+"way"
       elsif word[0..1].scan(/[aeiou]/).count > 0
         new_word = word[1..word.length]+word[0]+"ay"
-      else
+      elsif word[0..2].scan(/[aeiou]/).count > 0
         new_word = word[2..word.length]+word[0..1]+"ay"
+      else
+        new_word = word[3..word.length]+word[0..2]+"ay"
       end
       output << new_word
     end
