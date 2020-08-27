e67ce5d0c2379c3bd55976f1dd7489d2187f5145
Cody Alvarez <cody_alvarez19@outlook.com>
2020-08-27 17:19:16 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,9 +4,32 @@ class PigLatinizer
     # split string into individual parts
     a = string.split(" ")
     # piglatinize each word using piglatinize_word helper method
+    b = a.map { |word| piglatinize_word(word) }
     # join the words again into a string
+    b.join(" ")
   end
 
   def piglatinize_word(word)
-  end
+
+    first_letter = word[0].downcase
+
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+      "#{word}way"
+      # piglatinize word that starts with vowel
+    else
+      consonants = []
+      consonants << word[0]
+      # piglatinize word that starts with a constant
+      if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+        consonants << word[1]
+
+        if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+          consonants << word[2]
+
+        end
+      end
+    end
+
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end
 end
