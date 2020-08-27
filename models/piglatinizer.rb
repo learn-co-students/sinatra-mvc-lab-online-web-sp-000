4e6a9c03aa8f6eecfdc21a83d2376771924cdd20
Cody Alvarez <cody_alvarez19@outlook.com>
2020-08-27 14:35:57 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,12 @@
+class PigLatinizer
+
+  def piglatinize(string)
+    # split string into individual parts
+    a = string.split(" ")
+    # piglatinize each word using piglatinize_word helper method
+    # join the words again into a string
+  end
+
+  def piglatinize_word(word)
+  end
+end
