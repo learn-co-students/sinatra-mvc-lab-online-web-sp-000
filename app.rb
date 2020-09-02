f0049b498acc51c7eaa75e8da71e1b56785a6722
milos381 <milos_popovic@outlook.com>
2020-09-02 00:18:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require_relative 'config/environment'
 require_relative 'models/piglatinizer.rb'
-class App < Sinatra::Base
-    get '/' do
-        erb :user_input
-    end
-    post '/' do
-        @user = PigLatinizer.new(params[:user_text])
-        erb :user_result
-    end
-end
 
+class App < Sinatra::Base
+
+    get '/' do
+      erb :user_input
+    end
+  
+    post '/piglatinize' do
+      pl = PigLatinizer.new
+      @piglatin = pl.piglatinize(params[:user_phrase])
+      erb :results
+    end
+  
+  end
 # Build a form to take user input in user_input.erb. Show that form using a GET request at /.
 
 # Create a POST method in your controller (app.rb) to receive your form's params.
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,30 +1,37 @@
 class PigLatinizer
-    attr_reader :text
 
-    def initialize(text)
-        @text = text.downcase
+    def piglatinize(input_str)
+      input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
     end
-    def manipulation
-
-        arr = text.downcase.split(' ')
-        vowels = ["a", "e", "i", "o", "u"]
-        consonants = []
-        arr.each do |word|
-            if vowels.include? word[0]
-                word << "way"
-            elsif vowels.include?(word[1]) == false
-                consonants << word[0]
-                consonants << word[1]
-                "#{word[1..-1] + consonants.join() + "ay"}"
-            elsif vowels.include?(word[2]) == false
-                consonants << word[0]
-                consonants << word[1]
-                consonants << word[2]
-                "#{word[2..-1] + consonants.join() + "ay"}"
-            end
-        end
-                
-              
+  
+    private
+  
+    def consonant?(char)
+      !char.match(/[aAeEiIoOuU]/)
     end
-   
-end
+  
+    def piglatinize_word(word)
+      # word starts with vowel
+      if !consonant?(word[0])
+        word = word + "w"
+      # word starts with 3 consonants
+      elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+        word = word.slice(3..-1) + word.slice(0,3)
+      # word starts with 2 consonants
+      elsif consonant?(word[0]) && consonant?(word[1])
+        word = word.slice(2..-1) + word.slice(0,2)
+      # word starts with 1 consonant
+      else
+        word = word.slice(1..-1) + word.slice(0)
+      end
+      word << "ay"
+    end
+  
+    def piglatinize_sentence(sentence)
+      sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+    end
+  
+  
+  
+  
+  end
